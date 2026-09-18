7925d09708e40b09c625540a34e05892391abf93
SURESH SHERIKAR <sherikarsuresh795@gmail.com>
2026-09-18 10:21:45 +0530

    create a class and create an object and display all the class details

---
+++ pb1.py
@@ -0,0 +1,10 @@
+class student():
+    name=input("Name:")
+    rollno=input("roll no:")
+    marks=input("marks:")
+o1=student()
+print(o1.name)
+print(o1.rollno)
+print(o1.marks)
+               
+
